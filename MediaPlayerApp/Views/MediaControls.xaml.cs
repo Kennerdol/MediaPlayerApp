202a9f429bde547b0c44f0f98299b9339dea508d
Kennedy Mulenga <kenny.t.mule.jnr@gmail.com>
2025-09-11 00:01:34 +0200

    SongThumbnailAndDetails work in progress

--- MediaPlayerApp/Views/MediaControls.xaml.cs
+++ MediaPlayerApp/Views/MediaControls.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MediaPlayerApp.Views
 {
@@ -23,11 +12,16 @@ namespace MediaPlayerApp.Views
         public MediaControls()
         {
             InitializeComponent();
+
+            // Assign PlacementTarget in code
+            volumePopup.PlacementTarget = speakerButton;
+
             // Show popup on mouse enter
             speakerButton.MouseEnter += (s, e) => volumePopup.IsOpen = true;
 
             // Hide popup on mouse leave (optional: add delay if needed)
             volumePopup.MouseLeave += (s, e) => volumePopup.IsOpen = false;
+
 
         }
 
