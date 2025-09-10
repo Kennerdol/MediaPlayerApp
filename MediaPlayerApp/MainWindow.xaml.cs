d57df255247517fd0aab00b7627cda31e1ec363a
Kennedy Mulenga <kenny.t.mule.jnr@gmail.com>
2025-09-10 21:03:42 +0200

    Add project files.

---
+++ MediaPlayerApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediaElement.Volume = 0.5; // Default volume
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Media files|*.mp3;*.mp4;*.wav;*.wmv|All files|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaElement.Source = new Uri(openFileDialog.FileName);
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Pause();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Stop();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (mediaElement != null)
+                mediaElement.Volume = volumeSlider.Value;
+        }
+    }
+}
---
+++ MediaPlayerApp/Views/MediaControls.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayerApp.Views
+{
+    /// <summary>
+    /// Interaction logic for MediaControls.xaml
+    /// </summary>
+    public partial class MediaControls : UserControl
+    {
+        public MediaControls()
+        {
+            InitializeComponent();
+            // Show popup on mouse enter
+            speakerButton.MouseEnter += (s, e) => volumePopup.IsOpen = true;
+
+            // Hide popup on mouse leave (optional: add delay if needed)
+            volumePopup.MouseLeave += (s, e) => volumePopup.IsOpen = false;
+
+        }
+
+    }
+}
